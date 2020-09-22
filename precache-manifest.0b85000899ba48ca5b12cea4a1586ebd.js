f867d5585764304f529429250dbe986e640ef953
rahul <rahulkeshri09.rk@gmail.com>
2020-09-22 17:23:00 +0530

    Updates

---
+++ precache-manifest.0b85000899ba48ca5b12cea4a1586ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6411675f02a4d3c8f0c28b1a2381a811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe2a7ea79bf7e2d60e80",
+    "url": "/static/css/main.2b64cbc9.chunk.css"
+  },
+  {
+    "revision": "fa19ba45649efa745265",
+    "url": "/static/js/2.cd2faa76.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.cd2faa76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2a7ea79bf7e2d60e80",
+    "url": "/static/js/main.5d77c509.chunk.js"
+  },
+  {
+    "revision": "a8af963f82773d66876b",
+    "url": "/static/js/runtime-main.1b7c4484.js"
+  }
+]);
